57cb8098e7f2c25999e61e613147c9e9edb15baf
DirtyNightmare <Kewinn9800@hotmail.com>
2017-03-28 11:58:12 +0200

    Update MenuActivity.java

--- App/MenuActivity.java
+++ App/MenuActivity.java
@@ -8,26 +8,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
-import com.HardcodedDataGrid.datagridsamples.DataGridActivity;
 import com.ominidata.device.Options.OptionsActivity;
-import com.ominidata.device.io.Inventory;
-import com.ominidata.device.io.manuel_upload;
 import com.ominidata.device.scan.ScanActivity;
 
 
 public class MenuActivity extends Activity {
-    Button btn_scan,btn_inv,btn_option,buttonupload,btn_lager;
+    Button btn_scan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_activity);
-        btn_cam = (Button) findViewById(R.id.btn_cam);
-        btn_inv = (Button) findViewById(R.id.btn_inv);
-        btn_option = (Button) findViewById(R.id.btn_options);
-        buttonupload = (Button) findViewById(R.id.buttonupload);
-        btn_lager = (Button) findViewById(R.id.btn_lager);
+        btn_scan = (Button) findViewById(R.id.btn_scan);
 
 
         btn_scan.setOnClickListener(new View.OnClickListener(){
@@ -36,37 +28,6 @@ public class MenuActivity extends Activity {
                 Intent intent=new Intent(getApplication(),ScanActivity.class);
                 startActivity(intent);
 
-            }
-        });
-        btn_inv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(getApplication(),Inventory.class);
-                startActivity(intent);
-            }
-        });
-
-        btn_option.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(getApplication(),OptionsActivity.class);
-                startActivity(intent);
-
-            }
-        });
-        buttonupload.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(getApplication(),manuel_upload.class);
-                startActivity(intent);
-            }
-        });
-        btn_lager.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(getApplication(),DataGridActivity.class);
-                startActivity(intent);
             }
         });
 
